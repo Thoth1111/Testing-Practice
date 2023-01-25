dbdbce935575a1ef7ea336983471cbf358545e7d
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-25 16:47:20 +0300

    Complete task 2 tests

--- task1.test.js
+++ task1.test.js
@@ -1,4 +1,4 @@
-const stringLength = require('./task1')
+const stringLength = require('./task1.js')
 
 it('Character Count', () => {
 expect(stringLength("balloon")).toStrictEqual({ "a": 1, "b": 1, "l": 2, "n": 1, "o": 2 })
---
+++ task2.test.js
@@ -0,0 +1,5 @@
+const reverseString = require('./task2.js')
+
+it('Reversal', () => {
+    expect(reverseString("Hello")).toBe("olleH")
+})
