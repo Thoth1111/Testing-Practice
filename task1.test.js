f729aa8c49072b3a32378d6c2e3f49b6d8bf300d
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-25 16:50:11 +0300

    Add tests for task 1

--- task1.test.js
+++ task1.test.js
@@ -3,3 +3,12 @@ const stringLength = require('./task1.js')
 it('Character Count', () => {
 expect(stringLength("balloon")).toStrictEqual({ "a": 1, "b": 1, "l": 2, "n": 1, "o": 2 })
 }) 
+
+it('Character Count', () => {
+    expect(stringLength("label")).toStrictEqual({ "a": 1, "b": 1, "e": 1, "l": 2 })
+})
+
+it('Character Count', () => {
+    expect(stringLength("rascal")).toStrictEqual({ "a": 2, "c": 1, "l": 1, "r": 1, "s": 1 })
+}) 
+
