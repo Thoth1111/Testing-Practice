ee58db1b82991fb149714ce329d3d903797403fc
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-01-25 16:36:45 +0300

    Modify test

--- task1.test.js
+++ task1.test.js
@@ -1,4 +1,5 @@
 const stringLength = require('./task1')
 
 it('Character Count', () => {
-expect(stringLength('balloon')).toBe({ a: 1, b: 1, l: 2, n: 1, o: 2 })}) 
+expect(stringLength("balloon")).toStrictEqual({ "a": 1, "b": 1, "l": 2, "n": 1, "o": 2 })
+}) 
